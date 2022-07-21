64ec5dc70a985f9f154e9cb61f8f00965210eb01
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-21 16:13:51 +0200

    add price variant

--- js/app.js
+++ js/app.js
@@ -2,8 +2,25 @@ const inputKmElement = document.querySelector('input[name="user-km"]');
 const selectAgeElement = document.querySelector('select[name="user-age"]');
 
 const buttonElement = document.querySelector('button');
-console.log(inputKmElement, selectAgeElement, buttonElement);
+
+const PRICE_PER_KM = 0.21;
+const DISCOUNT_UNDER_18 = 0.2;
+const DISCOUNT_OVER_65 = 0.4;
 
 buttonElement.addEventListener('click', function () {
-    console.log(inputKmElement.value, selectAgeElement.value);
+    // calcolare il prezzo del biglietto base
+    const basePrice = parseFloat(inputKmElement.value) * PRICE_PER_KM;
+    let finalPrice;
+
+    if (selectAgeElement.value === 'under-18') {
+        // applicare sconto del 20%
+        finalPrice = basePrice * (1 - DISCOUNT_UNDER_18);
+    } else if (selectAgeElement.value === 'over-65') {
+        // applicare sconto del 40%
+        finalPrice = basePrice * (1 - DISCOUNT_OVER_65);
+    } else {
+        finalPrice = basePrice;
+    }
+
+    console.log(finalPrice);
 });
