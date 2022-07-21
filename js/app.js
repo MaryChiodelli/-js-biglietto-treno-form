ac007fc95215f86bb0687a5f73ed4689ec9e6914
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-21 19:20:21 +0200

    add style to form

--- js/app.js
+++ js/app.js
@@ -16,30 +16,34 @@ buttonElement.addEventListener('click', function () {
     const userName = inputNameElement.value;
     const userKm = parseFloat(inputKmElement.value);
     
-    // calcolare il prezzo del biglietto base
-    const basePrice = userKm * PRICE_PER_KM;
-    let finalPrice;
-    let ticketTipe;
-
-    if (selectAgeElement.value === 'under-18') {
-        // applicare sconto del 20%
-        finalPrice = basePrice * (1 - DISCOUNT_UNDER_18);
-        ticketTipe = 'Biglietto Ridotto under 18';
-    } else if (selectAgeElement.value === 'over-65') {
-        // applicare sconto del 40%
-        finalPrice = basePrice * (1 - DISCOUNT_OVER_65);
-        ticketTipe = 'Biglietto Ridotto over 65';
+    if ( userName === '' || userName === ' ' || userKm <= 0 ) {
+        alert('I dati inseriti non sono corretti.')
     } else {
-        finalPrice = basePrice;
-        ticketTipe = 'Biglietto Standard';
+        // calcolare il prezzo del biglietto base
+        const basePrice = userKm * PRICE_PER_KM;
+        let finalPrice;
+        let ticketTipe;
+    
+        if (selectAgeElement.value === 'under-18') {
+            // applicare sconto del 20%
+            finalPrice = basePrice * (1 - DISCOUNT_UNDER_18);
+            ticketTipe = 'Biglietto Ridotto under 18';
+        } else if (selectAgeElement.value === 'over-65') {
+            // applicare sconto del 40%
+            finalPrice = basePrice * (1 - DISCOUNT_OVER_65);
+            ticketTipe = 'Biglietto Ridotto over 65';
+        } else {
+            finalPrice = basePrice;
+            ticketTipe = 'Biglietto Standard';
+        }
+    
+        sectionTicketElement.style.display = 'block';
+    
+        tableElement.innerHTML = `
+        <td>${userName}</td>
+        <td>${ticketTipe}</td>
+        <td>5</td>
+        <td>92911</td>
+        <td>${finalPrice.toFixed(2)} €</td>`;
     }
-
-    sectionTicketElement.style.display = 'block';
-
-    tableElement.innerHTML = `
-    <td>${userName}</td>
-    <td>${ticketTipe}</td>
-    <td>5</td>
-    <td>92911</td>
-    <td>${finalPrice.toFixed(2)} €</td>`;
 });
