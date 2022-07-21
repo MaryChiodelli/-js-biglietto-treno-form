c860415e96f0801cb6403dfe5947db8e253eb9c1
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-21 15:17:41 +0200

    add click-event on button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const inputKmElement = document.querySelector('input[name="user-km"]');
+const selectAgeElement = document.querySelector('select[name="user-age"]');
+
+const buttonElement = document.querySelector('button');
+console.log(inputKmElement, selectAgeElement, buttonElement);
+
+buttonElement.addEventListener('click', function () {
+    console.log(inputKmElement.value, selectAgeElement.value);
+});
